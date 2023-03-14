0934ce1b3bfb0b9ea208c6175e09c15b5a251c65
Joshhortt <joshhortt@yahoo.com>
2023-03-14 19:49:27 +0000

    update: project paragraph tag description

--- pages/projects.js
+++ pages/projects.js
@@ -13,7 +13,8 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            Here I showcase my projects with a hero image (16 x 9), a title, a description and link
+            to Github project files.
           </p>
         </div>
         <div className="container py-12">
